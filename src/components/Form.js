1cbf04b9bd945c905fce591080e3ed6311a8e824
Gavin Stark <gavin@gstark.com>
2017-08-17 11:15:20 -0400

    Adds a simple form with a change handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,17 +11,28 @@ class Form extends React.Component {
     }
   }
 
-  handleNameChange = (event) => {
+  _nameChange = (event) => {
     const newName = event.target.value
 
     this.setState({ name: newName })
   }
 
+  _submit = (event) => {
+    // Don't follow the form's normal `ACTION` since
+    // we control everything, bwahahahahahaha
+    event.preventDefault()
+
+    // Eventually something this to send the data to the api:
+    // window.fetch('http://api.com/newName')
+    alert(`Thank you, ${this.state.name}`)
+  }
+
   render() {
     return (
-      <form>
-        <input onChange={this.handleNameChange}
+      <form onSubmit={this._submit}>
+        <input onChange={this._nameChange}
              name="name" type="text" value={this.state.name} />
+        <button type="submit">GO</button>
       </form>
     )
   }
